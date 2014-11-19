3d2f540a664a011330fb1e13af9af973111ccc15
kudzaiPius <kaypeedub@gmail.com>
2014-11-19 17:48:20 +0200

    Feature branch II first commit : refactored

---
+++ campus/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Lecturer(models.Model) :
+	name = models.CharField(max_length=200)
+	surname = models.CharField(max_length=200)
+
+	@property
+	def full_name(self):
+        	return "%s %s" % (self.name, self.surname)
+
+    	full_name = property(full_name)
+
+	def __unicode__(self): 
+        	return "%s %s" % (self.name, self.surname)
+
+	class Meta:
+    		unique_together = ('name', 'surname',)
+
+    
