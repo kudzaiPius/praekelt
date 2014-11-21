1bd2683c6700b373cc8743b3a90df5f9b40e0f82
kudzaiPius <kaypeedub@gmail.com>
2014-11-21 09:25:32 +0200

    Commit after first refactoring of .py files to match style guidelines

--- campus/admin.py
+++ campus/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from .models import Lecturer
 
-class LecturerAdmin(admin.ModelAdmin):
-    #fields = ('name', 'surname')
-    list_display = ('name', 'surname', 'full_name')
-    #ordering = ['full_name']#['name', 'surname']
 
-# Register your models here.
+class LecturerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'surname', 'full_name')
+
 admin.site.register(Lecturer, LecturerAdmin)
--- campus/models.py
+++ campus/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
 class Lecturer(models.Model) :
     name = models.CharField(max_length=200)
     surname = models.CharField(max_length=200)
