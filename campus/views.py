0983be844eddce2300f4e72c0ca171a34c3b3c48
hedleyroos <hedleyroos@gmail.com>
2014-12-05 12:09:12 +0200

    Reshuffle templates

--- campus/views.py
+++ campus/views.py
@@ -1,7 +1,9 @@
 from django.core.urlresolvers import reverse
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import Context, loader
+from django.template import RequestContext, Context, loader
+from django.shortcuts import get_object_or_404, render_to_response
+from django.contrib import messages
 
 from campus.models import Lecturer
 from campus.forms import messageLecturerForm
@@ -15,7 +17,7 @@ def admin_table(request):
      context = Context({
           "ordered_lecturers": ordered_lecturers,
      })
- 
+
      return HttpResponse(template.render(context))
 
 def message_lecturer(request):
@@ -24,13 +26,14 @@ def message_lecturer(request):
         form = messageLecturerForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
+            messages.success(request, "cool", fail_silently=True)
             return HttpResponseRedirect(reverse("message_lecturer"))
     else:
         form = messageLecturerForm()
 
     ordered_lecturers = Lecturer.objects.order_by("name", "surname")
-    context = Context({
-		"ordered_lecturers": ordered_lecturers,
-	})
-    return HttpResponse(loader.get_template("message_lecturer.html").render(
-        context), {"form":form})
+    return render_to_response(
+        "message_lecturer.html",
+        {"ordered_lecturers": ordered_lecturers},
+        context_instance=RequestContext(request)
+    )
